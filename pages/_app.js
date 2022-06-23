52098104a56cf908b2a659094fa8cee444e90730
naja-ep <naja@epart.com>
2022-06-23 14:01:59 +0900

    OneSignal 적용

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,26 @@
+import { useEffect } from 'react';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function() {
+      OneSignal.init({
+        appId: "516b2e1c-1f27-4c68-b639-b2d82617d697",
+        safari_web_id: "",
+        notifyButton: {
+          enable: true,
+            },
+
+            allowLocalhostAsSecureOrigin: true,
+        });
+    });
+
+    return () => {
+        window.OneSignal = undefined;
+    };
+}, []); // <-- run this effect once on mount
+
   return <Component {...pageProps} />
 }
 
