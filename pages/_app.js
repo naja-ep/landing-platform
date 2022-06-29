68b5a6ef7cbab6c6af6f84c5ac0fbc150ed4bb76
naja-ep <naja@epart.com>
2022-06-29 16:54:11 +0900

    external id 수정

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,16 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   
-  const externalUserId = '12345567'; //값 수정 필요
+  //const externalUserId = '12345567'; //값 수정 필요
 
   useEffect(() => {
     window.OneSignal = window.OneSignal || [];
     OneSignal.push(function() {
       OneSignal.init({
-         appId: "516b2e1c-1f27-4c68-b639-b2d82617d697", //실서버
-        //appId: "ed203017-82b0-43f9-ac75-e39079746cb5", //개발서버
+         //appId: "516b2e1c-1f27-4c68-b639-b2d82617d697", //실서버
+        appId: "ed203017-82b0-43f9-ac75-e39079746cb5", //개발서버
         safari_web_id: "",
         subdomainName: "당신의 서브도메인 여기에",
-        text:"텍스트입니다~",
         welcomeNotification : {
           disable:true,
           title:"웰컴알림의 제목",
@@ -21,13 +20,29 @@ function MyApp({ Component, pageProps }) {
         },
         notifyButton: {
           enable: true,
+          size: "medium", //large, medium, small
+          position: "bottom-right", //bottom-left, bottom-right(default)
         },
         allowLocalhostAsSecureOrigin: true,
       })
-      OneSignal.setExternalUserId(externalUserId);
-      OneSignal.getExternalUserId().then(function(externalUserId){
-        console.log("externalUserId: ", externalUserId);
-      });
+      OneSignal.push(["getNotificationPermission", function(permission) {
+        console.log("Site Notification Permission:", permission);
+        // (Output) Site Notification Permission: default
+      }]);
+      OneSignal.getUserId().then(function(userId){ //userId 가져오기
+        if(userId == null){
+          console.log('userId가 없습니다.')
+        } else {
+          console.log("userId(=playerId): ",userId)
+          const externalUserId = userId;
+          console.log("userId로 가져온 externalUserId: ",externalUserId);
+          OneSignal.setExternalUserId(externalUserId);
+          OneSignal.getExternalUserId().then(function(externalUserId){
+            console.log("서버에서 가져온 externalUserId: ", externalUserId);
+          });
+        }
+      })
+
 
     });
 
